e23087d7d0e56b428bee108980727e1559ab101d
Yuri Sabedot Venturin <ysventurin@ucs.br>
2024-08-25 18:54:22 -0300

    start Board and Piece classes

--- chess-system/src/tests/Tests.java
+++ chess-system/src/tests/Tests.java
@@ -1,11 +1,12 @@
 package tests;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Tests {
     public static void main(String[] args) {
         Position pos = new Position(3, 0);
-
+        Board board = new Board(8, 8);
         System.out.println(pos);
     }
 }
