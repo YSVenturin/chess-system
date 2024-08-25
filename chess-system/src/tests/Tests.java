436fc95ea19a0d77a9ac8ec5ddb7a59033143d58
Yuri Sabedot Venturin <ysventurin@ucs.br>
2024-08-25 18:43:15 -0300

    create class Position

---
+++ chess-system/src/tests/Tests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import boardgame.Position;
+
+public class Tests {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 0);
+
+        System.out.println(pos);
+    }
+}
